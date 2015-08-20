cd5554d4189c09c44a47a1d0c5426c128bab0ada
Sinar <sinar.shebl@phonidex.com>
2015-08-20 12:20:35 +0200

    Inserting, Adding and doing queries to couchbase lite

---
+++ My Address Book/Actions.swift
@@ -0,0 +1,13 @@
+//
+//  Actions.swift
+//  My Address Book
+//
+//  Created by Sinar Shebl on 8/20/15.
+//  Copyright (c) 2015 Sinar Shebl. All rights reserved.
+//
+
+import Cocoa
+
+class Actions: UIViewController {
+
+}
---
+++ My Address Book/AddContact.swift
@@ -0,0 +1,13 @@
+//
+//  AddContact.swift
+//  Pods
+//
+//  Created by Sinar Shebl on 8/19/15.
+//
+//
+
+import Cocoa
+
+class AddContact: UIViewController {
+
+}
