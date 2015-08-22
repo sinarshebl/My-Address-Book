a43556516b1deecd3ca0e0c8cb1150a94b927217
Sinar <sinar.shebl@phonidex.com>
2015-08-22 11:05:05 +0200

    Fixing some bugs, delete and add alerts

---
+++ My Address Book/ContactPhone.swift
@@ -0,0 +1,13 @@
+//
+//  ContactPhone.swift
+//  My Address Book
+//
+//  Created by Sinar Shebl on 8/22/15.
+//  Copyright (c) 2015 Sinar Shebl. All rights reserved.
+//
+
+import Cocoa
+
+class ContactPhone: UITableView {
+
+}
